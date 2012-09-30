d0284cc1c694ad3c2210ada58e824437dbb779c0
Mitchell Rivera <rivera.mitchell@gmail.com>
2012-09-29 19:36:34 -0700

    initial commit

---
+++ Fantasy Wars/Fantasy Wars/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Fantasy_Wars
+{
+    class Tile : DrawableGameComponent
+    {
+        private Color color;
+
+        private Vector3 position;
+        private Vector2 dimensions;
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
