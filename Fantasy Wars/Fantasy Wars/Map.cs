4eafb97392e53ccb7d3c66bc1bb0e65236ff288e
Mitchell Rivera <rivera.mitchell@gmail.com>
2012-09-29 22:30:39 -0700

    fill out draw methods

--- Fantasy Wars/Fantasy Wars/Map.cs
+++ Fantasy Wars/Fantasy Wars/Map.cs
@@ -3,16 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Fantasy_Wars
 {
     class Map : DrawableGameComponent
     {
         private Tile[,] tiles;
+        SpriteBatch spriteBatch;
 
         public Map(Game1 game) : base(game)
         {
-            
+        }
+
+        protected override void LoadContent()
+        {
+            this.spriteBatch = new SpriteBatch(Game.GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            var x = 0;
+            var y = 0;
+            var xLen = this.tiles.GetLength(0);
+            var yLen = this.tiles.GetLength(1);
+
+            while(x < xLen && y < yLen)
+            {
+                this.tiles[x, y].Draw(gameTime);
+                x++;
+                y--;
+
+                if(y < 0 || x >= xLen)
+                {
+                    y = x;
+                    if(y >= yLen)
+                    {
+                        x = y - yLen + 1;
+                        y = yLen - 1;
+                    }
+                    else
+                    {
+                        x = 0;
+                    }
+                }
+
+            }
+            base.Draw(gameTime);
         }
     }
 }
--- Fantasy Wars/Fantasy Wars/Tile.cs
+++ Fantasy Wars/Fantasy Wars/Tile.cs
@@ -9,23 +9,42 @@ namespace Fantasy_Wars
 {
     class Tile : DrawableGameComponent
     {
-        private Color color;
+        private readonly Color color;
 
-        private Vector3 position;
-        private Vector2 dimensions;
+        private readonly Vector3 position;
+        private readonly Vector2 dimensions = new Vector2(64, 32);
+        private readonly SpriteBatch spriteBatch;
 
-        public Tile(Game1 game) : base(game)
+        private Texture2D texture;
+
+        public Tile(Game1 game, Color color, Vector3 position, SpriteBatch spriteBatch) : base(game)
         {
-            
+            this.color = color;
+            this.position = position;
+            this.spriteBatch = spriteBatch;
         }
 
         protected override void LoadContent()
         {
+            var contentManager = this.Game.Content;
+
+            this.texture = contentManager.Load<Texture2D>("grass.png");
+
             base.LoadContent();
         }
 
         public override void Draw(GameTime gameTime)
         {
+            var x = this.position.X;
+            x *= this.dimensions.X;
+            // center it
+            x -= this.dimensions.X/2;
+
+            var y = this.position.Y;
+            y *= this.dimensions.Y;
+
+            this.spriteBatch.Draw(this.texture, new Vector2(x, y), this.color);
+
             base.Draw(gameTime);
         }
     }
